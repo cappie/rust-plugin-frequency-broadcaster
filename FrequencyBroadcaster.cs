a066ce30efab6f8790a1eb19fb22b8b1438fa541
cappie <cappie@users.noreply.github.com>
2023-07-26 22:23:59 +0200

    Update FrequencyBroadcaster.cs

--- FrequencyBroadcaster.cs
+++ FrequencyBroadcaster.cs
@@ -4,16 +4,26 @@ using Newtonsoft.Json;
 
 namespace Oxide.Plugins
 {
-	[Info("rust-plugin-frequency-broadcaster", "cappie", "1.0.0")]
-	[Description("Broadcast a frequency on a specific event.")]
-	internal class FrequencyBroadcaster : RustPlugin
+	[Info("Frequency Broadcaster", "cappie", "1.1.0")]
+	[Description("Broadcasts frequencies for specific events.")]
+	public class FrequencyBroadcaster : RustPlugin
 	{
+		#region Variables
+		//[PluginReference] Plugin TimeOfDay;
 		private PluginConfigData _configData;
+
+		//private bool Active;
+		private bool isDayTime = false;
+
+		private List<IRFObject> _dayFrequencyListeners;
 		private List<IRFObject> _nightFrequencyListeners;
+
+		#endregion
 
 		/// <summary>
 		///     Load default configuration.
 		/// </summary>
+		#region Data
 		protected override void LoadDefaultConfig()
 		{
 			Config.Settings.DefaultValueHandling = DefaultValueHandling.Populate;
@@ -42,29 +52,95 @@ namespace Oxide.Plugins
 
 		private void OnServerInitialized()
 		{
+			//VerifyDependency();
+			SetupListeners();
+		}
+
+		#endregion
+
+		#region Functions
+		/*
+        private void VerifyDependency()
+        {
+            if (TimeOfDay)
+                Active = true;
+            else
+            {
+                PrintWarning(GetMsg("noTOD"));
+                Active = false;
+            }
+        }
+		*/
+
+		private void SetupListeners() {
+			_dayFrequencyListeners = RFManager.GetListenList(_configData.DayFrequency);
 			_nightFrequencyListeners = RFManager.GetListenList(_configData.NightFrequency);
 		}
 
-		private void OnRfListenerAdded()
+		private void OnRfListenerAdded(IRFObject obj, int frequency)
 		{
+			SetupListeners();
+
+			// if the listener has tuned into the day frequency...
+			if (frequency == _configData.DayFrequency) {
+				if (isDayTime) {
+					foreach (var listener in _dayFrequencyListeners) {
+						listener.RFSignalUpdate(true);
+					}
+				}
+				Puts($"A listener for the day frequency was added.");
+			}
+			
+			// if the listener has tuned into the night frequency...
+			if (frequency == _configData.NightFrequency) {
+				if (!isDayTime) {
+					foreach (var listener in _nightFrequencyListeners) {
+						listener.RFSignalUpdate(true);
+					}
+				}
+				Puts($"A listener for the night frequency was added.");
+			}
 			OnServerInitialized();
 		}
 
-		private void OnRfListenerRemoved()
+		private void OnRfListenerRemove(IRFObject obj, int frequency)
 		{
+			if (frequency == _configData.DayFrequency) {
+				Puts($"A listener for the day frequency was removed.");
+			}
+			if (frequency == _configData.NightFrequency) {
+				Puts($"A listener for the night frequency was removed.");
+			}
 			OnServerInitialized();
 		}
 
 		private void OnTimeSunset()
 		{
-			foreach (var listener in _nightFrequencyListeners) listener.RFSignalUpdate(true);
+			isDayTime = false;
+			// timeComponent.DayLengthInMinutes = dayLength * (24.0f / (TOD_Sky.Instance.SunsetTime - TOD_Sky.Instance.SunriseTime));
+			foreach (var listener in _dayFrequencyListeners) {
+				listener.RFSignalUpdate(false);
+			}
+			foreach (var listener in _nightFrequencyListeners) {
+				listener.RFSignalUpdate(true);
+			}
+			Puts($"Stopped broadcasting at frequency {_configData.DayFrequency} because of sunset.");
 			Puts($"Started broadcasting at frequency {_configData.NightFrequency} because of sunset.");
 		}
 
 		private void OnTimeSunrise()
 		{
-			foreach (var listener in _nightFrequencyListeners) listener.RFSignalUpdate(false);
+			isDayTime = true;
+			
+			// timeComponent.DayLengthInMinutes = dayLength * (24.0f / (TOD_Sky.Instance.SunsetTime - TOD_Sky.Instance.SunriseTime));
+			foreach (var listener in _dayFrequencyListeners) {
+				listener.RFSignalUpdate(true);
+			}
+			foreach (var listener in _nightFrequencyListeners) {
+				listener.RFSignalUpdate(false);
+			}
 			Puts($"Stopped broadcasting at frequency {_configData.NightFrequency} because of sunrise.");
+			Puts($"Started broadcasting at frequency {_configData.DayFrequency} because of sunrise.");
 		}
 
 		/// <summary>
@@ -75,6 +151,30 @@ namespace Oxide.Plugins
 			[DefaultValue(4760)]
 			[JsonProperty(PropertyName = "nightFrequency")]
 			public int NightFrequency { get; set; }
+
+			[DefaultValue(4761)]
+			[JsonProperty(PropertyName = "dayFrequency")]
+			public int DayFrequency { get; set; }
 		}
+
+		#endregion
+
+        #region Messaging
+        private void SendMsg(BasePlayer player, string message, string keyword) => SendReply(player, $"<color=orange>{keyword}</color><color=#939393>{message}</color>");
+        private string GetMsg(string key) => lang.GetMessage(key, this);
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(Messages, this);
+        }
+        Dictionary<string, string> Messages = new Dictionary<string, string>
+        {
+            {"noTOD", "Unable to find TimeOfDay, unable to proceeed"},
+			{"started", "Started broadcasting at frequency {0} because of {1}."},
+			{"stopped", "Stopped broadcasting at frequency {0} because of {1}."},
+			{"sunrise", "sunrise"},
+			{"sunset", "sunset"}
+        };
+
+        #endregion
 	}
 }
